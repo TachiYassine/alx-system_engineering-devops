79053354313b69aee778cd07bee6ff8ee4ca190d
TachiYassine <tachi.yassine.3@gmail.com>
2024-05-08 08:43:48 -0700

    Repetition Token #1

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
+
 # This Ruby script takes an argument from the command line
 # and scans it for occurrences of the word "School", then
 # prints out those occurrences.
+
 puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+#done by Tachi Yassine
+
+puts ARGV[0].scan(/hb?tn/).join
